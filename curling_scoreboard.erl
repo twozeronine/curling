b0d1e0fd317b5f7ea744b45c26229b6d0df02854
twozeronine <amdusias209@gmail.com>
2023-01-16 14:35:01 +0900

    feat : curling_scoreboard

--- curling_scoreboard.erl
+++ curling_scoreboard.erl
@@ -6,9 +6,17 @@
 init([]) ->
     {ok, []}.
 
+handle_event({set_teams, TeamA, TeamB}, State) ->
+    curling_scoreboard_hw:set_teams(TeamA, TeamB),
+    {ok, State};
+handle_event({add_points, Team, N}, State) ->
+    [curling_scoreboard_hw:add_point(Team) || _ <- lists:seq(1,N)],
+    {ok, State};
+handle_event(next_round, State) ->
+    curling_scoreboard_hw:next_round(),
+    {ok, State};
 handle_event(_, State) ->
     {ok, State}.
-
 handle_call(_, State) ->
     {ok, ok, State}.
 
